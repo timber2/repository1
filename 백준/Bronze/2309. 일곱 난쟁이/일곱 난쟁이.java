f415644ffa236192b0306ffe1b0493d49211818f
오승윤 <92132316+timber2@users.noreply.github.com>
2025-06-23 15:35:48 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 68 ms, Memory: 11532 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,41 +1,34 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-		static int[] nanjaeng9 = new int [9];
-		static Vector nanjaeng7 = new Vector(7);
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int sum= 0;
-		for(int i = 0;i<9;i++) {
-			nanjaeng9[i] = Integer.parseInt(br.readLine());
-			sum+=nanjaeng9[i];
-		}
-		Loop1 :
-		for(int i=0;i<8;i++) {
-			Loop2 :
-			for(int j=i+1;j<9;j++) {
-				if(nanjaeng9[i] + nanjaeng9[j] == sum - 100) {
-					makeArray(i,j);
-					break Loop1;
-				}
-			}
-		}
-		Collections.sort(nanjaeng7);
-		for(var e : nanjaeng7) {
-			bw.write(e + "\n");
-		}
-		bw.flush();
-		bw.close();
-	}
-	static void makeArray(int i, int j) {
-		for(int a = 0 ; a < 9 ; a++) {
-			if(a == i || a == j) {
-				continue;
-			}
-			nanjaeng7.add(nanjaeng9[a]);
-		}
-		return;
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int[] array = new int [9];
+        int sum = 0;
+        for(int i= 0 ; i < 9;i++) {
+            array[i] = Integer.parseInt(br.readLine());
+            sum += array[i];
+        }
+        int i=0,j=0;
+        Arrays.sort(array);
+        Loop1:
+        for(i=0 ; i < 8 ; i++) {
+            for(j = i+1 ; j <9 ;j++) {
+                if(array[i] + array[j] == sum - 100) {
+                    break Loop1;
+                }
+            }
+        }
+        for(int k=0;k < 9 ;k++) {
+            if( k == i || k ==j) {
+                continue;
+            }
+            else {
+                sb.append(array[k] + "\n");
+            }
+        }
+        System.out.print(sb);
+    }
 }
