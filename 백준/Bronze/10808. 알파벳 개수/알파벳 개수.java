9af182909ff9cecdb97f3b61077aa1b780751a6a
오승윤 <92132316+timber2@users.noreply.github.com>
2025-06-25 18:16:18 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 64 ms, Memory: 11532 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -2,18 +2,18 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		String s = br.readLine();
-		int [] alphabet = new int[26];
-		for(int i=0;i<s.length();i++) {
-			alphabet[(int)s.charAt(i) -97]++;
-		}
-		for(int i=0;i<26;i++) {
-			sb.append(alphabet[i] + " ");
-		}
-		sb.deleteCharAt(sb.length()-1);
-		System.out.print(sb);
-	}
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        int[] alphabet = new int[26];
+        String str = br.readLine();
+        for(int i=0;i <str.length();i++) {
+            alphabet[(int)str.charAt(i)-97]++;
+        }
+        for(int i=0;i < alphabet.length;i++) {
+            sb.append(alphabet[i] + " ");
+        }
+        sb.deleteCharAt(sb.length()-1);
+        System.out.println(sb);
+    }
 }
