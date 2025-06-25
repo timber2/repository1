c7b4decab70dc6b7c31d32b7c0a96f57672c97b3
오승윤 <92132316+timber2@users.noreply.github.com>
2025-06-25 19:50:22 +0900

    [Bronze II] Title: 숫자의 개수, Time: 64 ms, Memory: 11476 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -1,24 +1,24 @@
 import java.util.*;
 import java.io.*;
 
+
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
-        int[] count = new int[10];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int num = 1;
-        for(int i=0;i <3;i++) {
-            int n = Integer.parseInt(br.readLine());
-            num *= n;
+        int[] array = new int[10];
+        for(int i=0;i <3 ;i++) {
+            num *= Integer.parseInt(br.readLine());
         }
-        while( num != 0 ) {
-            count[num%10]++;
+        // 32130248
+        while(num > 0) {
+            array[num%10]++;
             num /=10;
         }
-        for(var e : count) {
-            sb.append(e + "\n");
+        for(int i=0;i <10;i++) {
+            sb.append(array[i] + "\n");
         }
-        sb.deleteCharAt(sb.length()-1);
         System.out.print(sb);
     }
 }
