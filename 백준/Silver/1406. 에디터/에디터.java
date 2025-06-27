81b5165ff51a7e68f2f66b6bdc1f6b6d0a00c2ac
오승윤 <92132316+timber2@users.noreply.github.com>
2025-06-27 13:51:37 +0900

    [Silver II] Title: 에디터, Time: 656 ms, Memory: 145632 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -2,45 +2,47 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int count =0;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		String str = br.readLine();
-		LinkedList<Character> list = new LinkedList<>(Arrays.asList());
-		for(int i=0;i<str.length();i++) {
-			char c = str.charAt(i);
-			list.add(c);
-		}
-		ListIterator<Character> it = list.listIterator();
-		while(it.hasNext()) {
-			it.next();
-		}
-		int N = Integer.parseInt(br.readLine());
-		for(int i=0;i<N;i++) {
-			str= br.readLine();
-			StringTokenizer st = new StringTokenizer(str);
-			String s = st.nextToken();
-			if(s.equals("L") && it.hasPrevious()) {
-				it.previous();
-			}
-			else if(s.equals("D") && it.hasNext()) {
-				it.next();
-			}
-			else if(s.equals("B") && it.hasPrevious()) {
-				it.previous();
-				it.remove();
-			}
-			else if(s.equals("P")) { // P $
-				String s2 = st.nextToken();
-				char c = s2.charAt(0);
-				it.add(c);
-			}
-		}
-		it = list.listIterator();
-		while(it.hasNext()) {
-			sb.append(it.next() + "");
-		}
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb =  new StringBuilder();
+        StringTokenizer str;
+        String A,B;
+        String sen = br.readLine();
+        int M = Integer.parseInt(br.readLine());
+        LinkedList<Character> list =  new LinkedList<>();
+        for(int i=0;i < sen.length() ; i++) {
+            list.add(sen.charAt(i));
+        }
+        ListIterator<Character> it = list.listIterator();
+        while(it.hasNext()) {
+            it.next();
+        }
+       for(int i =0 ;i <M ;i++) {
+            str = new StringTokenizer(br.readLine());
+            A = str.nextToken();
+            if( A.equals("L")) {
+                if(it.hasPrevious()) it.previous();
+            }
+            else if(A.equals("D")) {
+                if(it.hasNext()) it.next();
+            }
+            else if(A.equals("B")) {
+                if(it.hasPrevious()) {
+                    it.previous();
+                    it.remove();
+                }
+            }
+            else {
+                B = str.nextToken();
+                it.add(B.charAt(0));
+            }
+       }
+       it = list.listIterator();
+       while(it.hasNext()) {
+           sb.append(it.next());
+       }
+
+
+       System.out.print(sb);
+    }
 }
