7ebe5d9e5be30688e154e8846db32c263fc720cb
오승윤 <92132316+timber2@users.noreply.github.com>
2025-06-27 16:19:07 +0900

    [Silver II] Title: 에디터, Time: 524 ms, Memory: 121252 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -1,7 +1,14 @@
+import java.awt.*;
 import java.util.*;
 import java.io.*;
 
 public class Main {
+    static int MX = 600001;
+    static char[] dat = new char[MX];
+    static int[] pre = new int[MX];
+    static int[] nxt = new int[MX];
+    static int unused = 1;
+    static int cursor = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb =  new StringBuilder();
@@ -9,40 +16,61 @@ public class Main {
         String A,B;
         String sen = br.readLine();
         int M = Integer.parseInt(br.readLine());
-        LinkedList<Character> list =  new LinkedList<>();
-        for(int i=0;i < sen.length() ; i++) {
-            list.add(sen.charAt(i));
+        Arrays.fill(pre,-1);
+        Arrays.fill(nxt,-1);
+        for(int i=0;i <sen.length();i++) {
+            dat[i+1] = sen.charAt(i);
+            pre[i+1] = i;
+            nxt[i] = i+1;
+            unused++;
+            cursor++;
         }
-        ListIterator<Character> it = list.listIterator();
-        while(it.hasNext()) {
-            it.next();
-        }
-       for(int i =0 ;i <M ;i++) {
+        //printArr();
+        for(int i= 0 ; i < M ;i++) {
             str = new StringTokenizer(br.readLine());
             A = str.nextToken();
-            if( A.equals("L")) {
-                if(it.hasPrevious()) it.previous();
+            if(A.equals("L")) {
+                if(pre[cursor] != -1) cursor = pre[cursor];
             }
             else if(A.equals("D")) {
-                if(it.hasNext()) it.next();
+                if(nxt[cursor] != -1) cursor = nxt[cursor];
             }
             else if(A.equals("B")) {
-                if(it.hasPrevious()) {
-                    it.previous();
-                    it.remove();
-                }
+                if(pre[cursor] != -1) erase(cursor);
             }
             else {
                 B = str.nextToken();
-                it.add(B.charAt(0));
+                insert(cursor, B.charAt(0));
             }
-       }
-       it = list.listIterator();
-       while(it.hasNext()) {
-           sb.append(it.next());
-       }
 
 
-       System.out.print(sb);
+        }
+        int cur = nxt[0];
+        while(cur !=-1) {
+            sb.append(dat[cur]);
+            cur = nxt[cur];
+        }
+        System.out.print(sb);
+    }
+    static void insert(int addr, char c) {
+        dat[unused] = c;
+        pre[unused] = addr;
+        nxt[unused] = nxt[addr];
+        if(nxt[addr] != -1) pre[nxt[addr]] = unused;
+        nxt[addr] = unused;
+        cursor = unused++;
+    }
+    static void erase(int addr) {
+        if(nxt[addr] != -1) pre[nxt[addr]] = pre[addr];
+        nxt[pre[addr]] = nxt[addr];
+        cursor = pre[addr];
+    }
+    static void printArr() {
+        int cur = nxt[0];
+        while(cur != -1) {
+            System.out.print(dat[cur] + " ");
+            cur = nxt[cur];
+        }
+        System.out.println(cursor);
     }
 }
