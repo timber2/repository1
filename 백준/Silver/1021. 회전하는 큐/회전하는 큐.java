cc8d34b46612a57cc6040ea8bbd35c5811a5151a
오승윤 <92132316+timber2@users.noreply.github.com>
2025-06-28 12:11:15 +0900

    [Silver III] Title: 회전하는 큐, Time: 72 ms, Memory: 11684 KB -BaekjoonHub

---
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
@@ -0,0 +1,43 @@
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        Deque<Integer> q = new LinkedList<>();
+        for(int i=1 ;i  <= N ;i++) q.add(i);
+        int first, target, left,right, count = 0;
+        st = new StringTokenizer(br.readLine());
+        while(st.hasMoreTokens()) {
+            first = q.getFirst();
+            target = Integer.parseInt(st.nextToken());
+            // 좌측으로 회전
+            left = 0;
+            while(q.getFirst() != target) {
+                q.addLast(q.getFirst());
+                q.removeFirst();
+                left++;
+            }
+            while(q.getFirst() != first) {
+                q.addLast(q.getFirst());
+                q.removeFirst();
+            }
+            // 오른쪽으로 회전
+            right = 0;
+            while(q.getFirst() != target) {
+                q.addFirst(q.getLast());
+                q.removeLast();
+                right++;
+
+            }
+            q.removeFirst();
+            count += (left>right) ? right : left;
+        }
+        sb.append(count);
+        System.out.print(sb);
+    }
+}
