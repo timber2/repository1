b1758b5589066a7bd241a2b9c0656fd7a386059c
오승윤 <92132316+timber2@users.noreply.github.com>
2025-06-29 22:59:46 +0900

    [Silver II] Title: 스택 수열, Time: 252 ms, Memory: 27712 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,43 +1,31 @@
-import java.util.*;
 import java.io.*;
+import java.nio.file.FileSystemNotFoundException;
+import java.util.*;
+
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	public static void main(String[] args) throws IOException {
-		Stack<Integer> S = new Stack<>();
-		StringBuilder sb = new StringBuilder();
-		// 숫자를 입력받음 ex) 4 3 6 8 7 5 2 1 
-		int n = Integer.parseInt(br.readLine());
-		int it = 0; // 현재 숫자
-		for(int i=0;i<n;i++) {
-			int num = Integer.parseInt(br.readLine());
-			if(num >=it) {
-				while(num !=it) {
-					it++;
-					S.add(it);
-					sb.append("+\n");
-				}
-				S.pop();
-				sb.append("-\n");
-			}
-			else {  // num이 현재 it보다 작은 경우에는
-				if(S.empty()) {
-					System.out.print("NO");
-					System.exit(0);
-				}
-				while(S.peek() != num) {
-					S.pop();
-					sb.append("-\n");
-					if(S.empty()) {
-						System.out.print("NO");
-						System.exit(0);
-					}
-				}
-				S.pop();
-				sb.append("-\n");
-			}
-		}
-		sb.deleteCharAt(sb.length()-1);
-		System.out.print(sb);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> s = new Stack<>();
+        int num, max = 0, N = Integer.parseInt(br.readLine());
+        for(int i=0; i<N; i++) {
+            num = Integer.parseInt(br.readLine());
+            if(num > max) {
+                while(max !=num) {
+                    s.push(++max);
+                    sb.append("+\n");
+                }
+            }
+            while(true) {
+                sb.append("-\n");
+                if(s.isEmpty()) {
+                    System.out.print("NO");
+                    return;
+                }
+                if(s.pop() == num) break;
+            }
+        }
+        System.out.print(sb);
+    }
 }
