2730e6aae2d87b15a22b6dc28c13e19ccfa6d680
오승윤 <92132316+timber2@users.noreply.github.com>
2025-07-01 10:22:35 +0900

    [Silver IV] Title: 큐 2, Time: 1116 ms, Memory: 341648 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.java
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -4,14 +4,13 @@ import java.io.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static StringBuilder sb;
+    static StringBuilder sb = new StringBuilder();
     static StringTokenizer str;
     static int N, last, size =0;
     static Queue<Integer> q = new LinkedList<>();
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
         while(N-- >0 ) {
-            sb = new StringBuilder();
             str = new StringTokenizer(br.readLine());
             switch (str.nextToken()) {
                 case "push":
@@ -20,34 +19,30 @@ public class Main {
                     size++;
                     break;
                 case "pop":
-                    if(size ==0) sb.append("-1");
+                    if(size ==0) sb.append("-1\n");
                     else  {
-                        sb.append(q.remove() );
+                        sb.append(q.remove()+"\n");
                         size--;
                     }
-                    bw.write(sb.toString() + "\n");
                     break;
                 case "size":
-                    sb.append(size);
-                    bw.write(sb.toString() + "\n");
+                    sb.append(size + "\n");
                     break;
                 case "empty":
-                    if(size ==0) sb.append("1");
-                    else sb.append("0");
-                    bw.write(sb.toString() + "\n");
+                    if(size ==0) sb.append("1\n");
+                    else sb.append("0\n");
                     break;
                 case "front" :
-                    if(size ==0) sb.append("-1");
-                    else sb.append(q.peek());
-                    bw.write(sb.toString() + "\n");
+                    if(size ==0) sb.append("-1\n");
+                    else sb.append(q.peek() + "\n");
                     break;
                 default :
-                    if(size ==0) sb.append("-1");
-                    else sb.append(last);
-                    bw.write(sb.toString() + "\n");
+                    if(size ==0) sb.append("-1\n");
+                    else sb.append(last + "\n");
                     break;
             }
         }
-        bw.flush();
+        bw.write(sb.toString());
+        bw.close();
     }
 }
