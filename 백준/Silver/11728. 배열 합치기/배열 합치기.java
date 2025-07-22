60dcaa593a1a47ef98baae0747fa4716e121ec32
오승윤 <92132316+timber2@users.noreply.github.com>
2025-07-22 11:03:07 +0900

    [Silver V] Title: 배열 합치기, Time: 1128 ms, Memory: 345048 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -2,46 +2,44 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
-    static int N , M;
-    static int[] array1,array2,result;
+    static int N,M;
+    static int[] array1 ,array2, result;
+    static int index1= 0,index2=0, index3=0;
     public static void main(String[] args) throws IOException {
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         array1 = new int[N];
         array2 = new int[M];
-        result = new int[N+M];
+        result = new int[N + M];
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i < N ;i++) {
-            array1[i] = Integer.parseInt(st.nextToken());
+        for(int i=0;i < N;i++) {
+            array1[i] =Integer.parseInt(st.nextToken());
         }
         st = new StringTokenizer(br.readLine());
-        for(int i=0;i < M ;i++) {
-            array2[i] = Integer.parseInt(st.nextToken());
+        for(int i=0; i < M;i++) {
+            array2[i] =Integer.parseInt(st.nextToken());
         }
-        int index1 =0, index2 =0;
-        for(int i =0; i < N + M ;i++) {
-            if(array1[index1] < array2[index2]) result[i] = array1[index1++];
-            else result[i] = array2[index2++];
+        while(index1 != N && index2 != M) {
+            if(array1[index1] < array2[index2]) {
+                result[index3++] = array1[index1++];
+            }
+            else {
+                result[index3++] = array2[index2++];
+            }
+        }
 
-            if(index1 == N) {
-                for(int j=i+1 ; j < M+ N ;j++) {
-                    result[j] = array2[index2++];
-                }
-                break;
-            }
-            else if(index2 ==M) {
-                for(int j=i+1 ; j < M+ N ;j++) {
-                    result[j] = array1[index1++];
-                }
-                break;
-            }
+        while(index1 != N ) {
+            result[index3++] = array1[index1++];
         }
-        for(int i=0;i < N+M;i++) {
-            sb.append(result[i] + " ");
+        while(index2 != M) {
+            result[index3++] = array2[index2++];
+        }
+        for(int e : result) {
+            sb.append(e).append(" ");
         }
         System.out.print(sb);
     }
