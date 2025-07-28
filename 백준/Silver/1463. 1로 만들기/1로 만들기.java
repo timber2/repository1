88163e5801b5642b4c0284d63264911c287d156b
오승윤 <92132316+timber2@users.noreply.github.com>
2025-07-28 11:24:50 +0900

    [Silver III] Title: 1로 만들기, Time: 112 ms, Memory: 16004 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -2,35 +2,31 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int [] dp;
-	static int N;
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	public static void main(String[] args) throws IOException {
-		StringBuilder sb = new StringBuilder();
-		N = Integer.parseInt(br.readLine());
-		dp = new int [N+1];
-		Arrays.fill(dp,  -1);
-		dp[1] = 0;
-		if(N >=2) {
-			dp[2] =1;
-			if(N>=3) {
-				dp[3] = 1;
-			}
-		}
-		for(int i=4;i<=N;i++) {
-			dp[i] = func(i) + 1;
-		}
-		sb.append(dp[N]);
-		System.out.print(sb);
-	}
-	static int func(int i) {
-		int a= dp[i/3],b = dp[i/2],c = dp[i-1];
-		if(i%3!=0) {
-			a = Integer.MAX_VALUE;
-		}
-		if(i%2!=0) {
-			b = Integer.MAX_VALUE;
-		}
-		return Math.min(Math.min(a,b),c);
-	}
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static int X;
+    static int[] array;
+    public static void main(String[] args) throws IOException {
+        X = Integer.parseInt(br.readLine());
+        array = new int[X+1];
+        dp(X);
+        sb.append(array[X]);
+        System.out.print(sb);
+    }
+    public static void dp(int X) {
+        array[1] = 0;
+        if(X >=2 ) array[2] = 1;
+        if(X>=3 ) array[3] =1;
+        for(int i=2; i<X ;i++) {
+            if(i*2 <= X && (array[i*2] == 0 || array[i*2] > array[i]+1 )){
+                array[i*2] = array[i] +1;
+            }
+            if(i*3 <= X && (array[i*3] == 0 || array[i*3] > array[i]+1 )) {
+                array[i*3] = array[i] +1;
+            }
+            if(i + 1 <= X && (array[i+1] == 0 || array[i+1] > array[i]+1 )) {
+                array[i+1] = array[i]+1;
+            }
+        }
+    }
 }
