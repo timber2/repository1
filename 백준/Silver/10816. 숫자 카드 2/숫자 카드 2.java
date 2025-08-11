1c69bcd4c87270bad16ebcb98eb9d88512ff7baa
오승윤 <92132316+timber2@users.noreply.github.com>
2025-08-11 11:35:50 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 960 ms, Memory: 182044 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,23 +1,42 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int[] array = new int [20000001];
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int N = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=0;i<N;i++) {
-			int A = Integer.parseInt(st.nextToken());
-			array[A+10000000]++;
-		}
-		int M = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
-		for(int i=0;i<M;i++) {
-			int B = Integer.parseInt(st.nextToken());
-			sb.append(array[B+10000000] + " ");
-		}
-		System.out.print(sb);
-	}
+    static BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    static int N,M;
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        arr = new int [N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i< N ;i++) arr[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(arr);
+        M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        while(st.hasMoreTokens()) {
+            int target = Integer.parseInt(st.nextToken());
+            sb.append(endIndex(target) - startIndex(target)).append(" ");
+        }
+        System.out.print(sb);
+    }
+    public static int startIndex(int target) {
+        int mid, st = 0, en = arr.length;
+        while(st < en) {
+            mid = (st+en)/2;
+            if(arr[mid] >= target) en = mid;
+            else st = mid + 1;
+        }
+        return st;
+    }
+    public static int endIndex(int target) {
+        int mid, st = 0, en = arr.length;
+        while(st < en) {
+            mid = (st+en)/2;
+            if(arr[mid] > target) en = mid;
+            else st = mid + 1;
+        }
+        return st;
+    }
 }
