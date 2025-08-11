33093a4d8cf240a7bc6f31b3b106ceabcb0a2afa
오승윤 <92132316+timber2@users.noreply.github.com>
2025-08-11 11:46:58 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 740 ms, Memory: 255264 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -5,38 +5,19 @@ public class Main {
     static BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
-    static int N,M;
+    static int N,M, bound = 10_000_000;
     static int[] arr;
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
-        arr = new int [N];
+        arr = new int [20_000_001];
         st = new StringTokenizer(br.readLine());
-        for(int i=0;i< N ;i++) arr[i] = Integer.parseInt(st.nextToken());
-        Arrays.sort(arr);
+        for(int i=0;i< N ;i++) {
+            int index = Integer.parseInt(st.nextToken());
+            arr[index + bound]++;
+        }
         M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        while(st.hasMoreTokens()) {
-            int target = Integer.parseInt(st.nextToken());
-            sb.append(endIndex(target) - startIndex(target)).append(" ");
-        }
+        while(st.hasMoreTokens()) sb.append(arr[bound + Integer.parseInt(st.nextToken())]).append(" ");
         System.out.print(sb);
-    }
-    public static int startIndex(int target) {
-        int mid, st = 0, en = arr.length;
-        while(st < en) {
-            mid = (st+en)/2;
-            if(arr[mid] >= target) en = mid;
-            else st = mid + 1;
-        }
-        return st;
-    }
-    public static int endIndex(int target) {
-        int mid, st = 0, en = arr.length;
-        while(st < en) {
-            mid = (st+en)/2;
-            if(arr[mid] > target) en = mid;
-            else st = mid + 1;
-        }
-        return st;
     }
 }
