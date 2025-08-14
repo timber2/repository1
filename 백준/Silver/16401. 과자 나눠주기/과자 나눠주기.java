544767c4139cc3445bafb3102e2a3a111782145f
오승윤 <92132316+timber2@users.noreply.github.com>
2025-08-14 10:55:24 +0900

    [Silver II] Title: 과자 나눠주기, Time: 512 ms, Memory: 162756 KB -BaekjoonHub

--- 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.java
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.java
@@ -4,34 +4,35 @@ import java.util.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static int N,M;
-    static long max = 0;
-    static long[] arr;
+    static int N,M, result =0, max =0;
+    static int[] arr;
     public static void main(String[] args) throws IOException {
         st = new StringTokenizer(br.readLine());
         M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
-        arr = new long [N];
+        arr = new int [N];
         st = new StringTokenizer(br.readLine());
-        for(int i=0;i < N ;i++) arr[i] = Long.parseLong(st.nextToken());
+        for(int i=0;i < N ;i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(max,arr[i]);
+        }
         func(M);
-        System.out.print(max);
+        System.out.print(result);
     }
     public static void func(int M) {
-        int mid, st = 1, en = 1_000_000_000;
-        long cnt;
-        while(st < en) {
+        int cnt, mid, st = 1, en = max;
+        while(st <= en) {
             mid = (st+en)/2;
             cnt = calCnt(mid);
-            if(cnt  < M) en = mid;
+            if(cnt  < M) en = mid -1;
             else {
-                max = mid;
+                result = mid;
                 st = mid + 1;
             }
         }
     }
-    public static long calCnt(long length) {
-        long cnt =0;
+    public static int calCnt(int length) {
+        int cnt =0;
         for(int i=0;i < N; i++) cnt += arr[i]/length;
         return cnt;
     }
