c19f266d5b9ab9ae917e8f539c8349b9088cc738
오승윤 <92132316+timber2@users.noreply.github.com>
2026-02-19 09:54:39 +0900

    [Silver III] Title: 두 수의 합, Time: 192 ms, Memory: 26908 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,30 +1,23 @@
-import java.util.*;
 import java.io.*;
-
+import java.util.*;
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    static int N, num, cnt =0;
+    static boolean[] arr;
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
-        int[] array = new int [2000001];
-        StringTokenizer str = new StringTokenizer(br.readLine());
-        while(str.hasMoreTokens()){
-            int a = Integer.parseInt(str.nextToken());
-            array[a] = 1;
+        sb = new StringBuilder();
+        N = Integer.parseInt(br.readLine());
+        arr = new boolean [2_000_001];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i < N ;i++) arr[Integer.parseInt(st.nextToken())] = true;
+        num = Integer.parseInt(br.readLine());
+        for(int i=1;i < num ;i++) {
+            if(arr[i] && arr[num-i]) cnt++;
         }
-        int count =0;
-        int x = Integer.parseInt(br.readLine());
-        for(int i=0;i < array.length;i++) {
-            if(x -i >=0 && array[i] == 1) {
-                array[i] = 0;
-                if(array[x-i] ==1 ) {
-                    count++;
-                }
-                array[x-i]= 0;
-            }
-        }
-        sb.append(count + "");
-        System.out.print(sb);
+        sb.append(cnt/2);
+        System.out.print(sb.toString());
     }
 }
