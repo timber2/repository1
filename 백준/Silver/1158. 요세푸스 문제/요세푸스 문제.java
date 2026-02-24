ceab21f193c888651217c832186bbe7c23e4615e
오승윤 <92132316+timber2@users.noreply.github.com>
2026-02-24 09:54:16 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 204 ms, Memory: 14776 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -5,20 +5,25 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
-    static int N,K;
-    static ArrayDeque<Integer> q = new ArrayDeque<>();
+    static LinkedList<Integer> list = new LinkedList<>();
+    static ListIterator<Integer> it;
+    static int N,K, num;
     public static void main(String[] args) throws IOException {
         sb.append("<");
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        for(int i=1;i <=N ;i++) q.add(i);
-        while(!q.isEmpty()) {
-            for(int i=0;i <K-1; i++) q.add(q.removeFirst());
-            sb.append(q.removeFirst() + ", ");
+        for(int i=1;i <= N ;i++) list.add(i);
+        it = list.listIterator();
+        while(!list.isEmpty()) {
+            for(int i=0;i < K ;i++) {
+                if(!it.hasNext()) it = list.listIterator();
+                num = it.next();
+            }
+            it.remove();
+            sb.append(num + ", ");
         }
-        sb.deleteCharAt(sb.length()-1);
-        sb.deleteCharAt(sb.length()-1);
+        sb = new StringBuilder(sb.substring(0, sb.length()-2));
         sb.append(">");
         System.out.print(sb.toString());
     }
