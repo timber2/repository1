da0bea752dae159b01b9dc70c745aeba4b75fc92
오승윤 <92132316+timber2@users.noreply.github.com>
2026-02-24 09:19:49 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 228 ms, Memory: 15000 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,30 +1,25 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int[] array = new int [20000001];
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		sb.append("<");
-		StringTokenizer str = new StringTokenizer(br.readLine());
-		Queue<Integer> Q = new LinkedList<>();
-		int N = Integer.parseInt(str.nextToken());
-		int K = Integer.parseInt(str.nextToken()) -1;
-		for(int i=0;i<N;i++) {
-			Q.add(i+1);
-		}
-		while(N-->0) {
-			for(int i=0;i<K;i++) {
-				Q.add(Q.peek());
-				Q.remove();
-			}
-			sb.append(Q.peek()+ ", ");
-			Q.remove();
-		}
-		sb.deleteCharAt(sb.length()-1);
-		sb.deleteCharAt(sb.length()-1);
-		sb.append(">");
-		System.out.print(sb);
-	}
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    static int N,K;
+    static ArrayDeque<Integer> q = new ArrayDeque<>();
+    public static void main(String[] args) throws IOException {
+        sb.append("<");
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        for(int i=1;i <=N ;i++) q.add(i);
+        while(!q.isEmpty()) {
+            for(int i=0;i <K-1; i++) q.add(q.removeFirst());
+            sb.append(q.removeFirst() + ", ");
+        }
+        sb.deleteCharAt(sb.length()-1);
+        sb.deleteCharAt(sb.length()-1);
+        sb.append(">");
+        System.out.print(sb.toString());
+    }
 }
